f12a8644a1c6536ae3ce2cfa0eba52eb1439c74c
ValeriaD18 <58814320+ValeriaD18@users.noreply.github.com>
2023-12-29 13:37:12 +0300

    Add files via upload

---
+++ dotnet/PgConnect/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Npgsql;
+using System;
+using System.IO;
+
+Console.WriteLine("User is ?");
+var userName = Console.ReadLine();
+Console.WriteLine("Password is ?");
+var password = Console.ReadLine();
+Console.WriteLine($"User is {userName}");
+Console.WriteLine($"Password is {password}");
+
+StreamReader sr = new StreamReader("../../../sdl.conf");
+
+var connectionString = sr.ReadLine();
+
+Npgsql.NpgsqlConnectionStringBuilder csb = new Npgsql.NpgsqlConnectionStringBuilder(connectionString);
+
+csb.Username = userName;
+csb.Password = password;
+
+try {
+    await using var dataSource = NpgsqlDataSource.Create(csb);
+
+    // Retrieve all rows
+    await using (var cmd = dataSource.CreateCommand("SELECT VERSION();"))
+    await using (var reader = await cmd.ExecuteReaderAsync())
+    {
+        while (await reader.ReadAsync())
+        {
+            Console.WriteLine(reader.GetString(0));
+        }
+    }
+}
+
+catch (Npgsql.PostgresException) {
+    Console.WriteLine("Неправильный логин или пароль");
+}
